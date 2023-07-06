0c7d8bc40137b656c71c21cc5b3fe56502ee2a4c
Rossywan França <rossyfranca@gmail.com>
2023-07-06 14:31:53 -0300

    project hr-payroll created

---
+++ hr-payroll/src/test/java/com/franca/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franca.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
