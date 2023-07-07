e5814cf76aebe5dc45ee385918ff0962d832c05d
Rossywan França <rossyfranca@gmail.com>
2023-07-07 14:16:35 -0300

    eureka server configured successfully

--- hr-payroll/src/main/java/com/franca/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/franca/hrpayroll/HrPayrollApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
-@RibbonClient(name = "hr-worker")
 @EnableFeignClients
 @SpringBootApplication
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
