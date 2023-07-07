76c9edd4a860fd3260e6e0dfaf0a32298be8e724
Rossywan França <rossyfranca@gmail.com>
2023-07-07 20:05:24 -0300

    resilience4j implemented

--- hr-payroll/src/main/java/com/franca/hrpayroll/controllers/PaymentController.java
+++ hr-payroll/src/main/java/com/franca/hrpayroll/controllers/PaymentController.java
@@ -2,7 +2,9 @@ package com.franca.hrpayroll.controllers;
 
 import com.franca.hrpayroll.entities.Payment;
 import com.franca.hrpayroll.services.PaymentService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,10 +17,17 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
+    @CircuitBreaker(name = "payment", fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
-    public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
-        Payment payment = paymentService.getPayment(workerId, days);
+    public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 
+        Payment payment = paymentService.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
+
+    }
+
+    public ResponseEntity<String> getPaymentAlternative(Long workerId, Integer days, Throwable t) {
+        System.out.println("Doesnt work");
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Error");
     }
 }
