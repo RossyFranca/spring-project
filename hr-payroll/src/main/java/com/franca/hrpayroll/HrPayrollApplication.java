f5e41d683a20f1dd8c7515102cbec55596dfb115
Rossywan França <rossyfranca@gmail.com>
2023-07-07 13:34:06 -0300

    hr-eureka-server created

--- hr-payroll/src/main/java/com/franca/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/franca/hrpayroll/HrPayrollApplication.java
@@ -3,9 +3,11 @@ package com.franca.hrpayroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
+@RibbonClient(name = "hr-worker")
 @EnableFeignClients
 @SpringBootApplication
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
