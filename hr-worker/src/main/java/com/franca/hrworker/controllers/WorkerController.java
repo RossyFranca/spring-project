c86ad6a9de951c64e87b1f56c418f084aa15c338
Rossywan França <rossyfranca@gmail.com>
2023-07-07 13:26:17 -0300

    add ribbon load balance, doesnt work untill

--- hr-worker/src/main/java/com/franca/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/franca/hrworker/controllers/WorkerController.java
@@ -2,7 +2,10 @@ package com.franca.hrworker.controllers;
 
 import com.franca.hrworker.entities.Worker;
 import com.franca.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,19 +16,26 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/workers")
-public class WorkerResources {
+public class WorkerController {
+
+    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerRepository repository;
 
     @GetMapping(value = "/")
-    public ResponseEntity<List<Worker>> findAll(){
-         List<Worker> list = repository.findAll();
-         return ResponseEntity.ok(list);
+    public ResponseEntity<List<Worker>> findAll() {
+        List<Worker> list = repository.findAll();
+        return ResponseEntity.ok(list);
 
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Worker> findById(@PathVariable Long id){
+    public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+        logger.info("PORT = " + env.getProperty("local.server.port"));
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
 
